e3d707fc9431366f2f8439c655ece2316c636a96
Quimey <quimeymata@gmail.com>
2023-02-08 12:55:03 -0300

    Adds Container UI component

---
+++ src/components/UI/Container/Container.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import classes from './Container.module.css';
+
+const Container = (props) => {
+  const containerClasses = `${classes.container} ${props.className}`;
+
+  return <div className={containerClasses}>{props.children}</div>;
+};
+
+export default Container;
