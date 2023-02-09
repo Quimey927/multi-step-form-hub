949e91d41a5538f69ed03cfab38089e6a9628825
Quimey <quimeymata@gmail.com>
2023-02-09 15:40:21 -0300

    Adds number current-step styles

--- src/components/Sidebar/Step/Step.js
+++ src/components/Sidebar/Step/Step.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import classes from './Step.module.css';
 
 const Step = (props) => {
+  const step = useSelector((state) => state.step.step);
+
+  const numberClasses =
+    step === props.number
+      ? `${classes.number} ${classes.active}`
+      : `${classes.number}`;
+
   return (
     <div className={classes.step}>
-      <div className={classes.number}>{props.number}</div>
+      <div className={numberClasses}>{props.number}</div>
       <div className={classes.info}>
         <span>{`Step ${props.number}`}</span>
         <span className={classes.text}>{props.text}</span>
