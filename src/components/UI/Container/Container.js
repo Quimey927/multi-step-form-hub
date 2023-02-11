85d6bfa50c7ef68815266b4df028f76a1035b100
Quimey <quimeymata@gmail.com>
2023-02-11 18:29:17 -0300

    Adds generic onClick property on Container comp

--- src/components/UI/Container/Container.js
+++ src/components/UI/Container/Container.js
@@ -5,7 +5,11 @@ import classes from './Container.module.css';
 const Container = (props) => {
   const containerClasses = `${classes.container} ${props.className}`;
 
-  return <div className={containerClasses}>{props.children}</div>;
+  return (
+    <div className={containerClasses} onClick={props.onClick}>
+      {props.children}
+    </div>
+  );
 };
 
 export default Container;
