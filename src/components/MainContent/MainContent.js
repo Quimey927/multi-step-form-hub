354bac785e213d924b9ab55433f47326e176c9bf
Quimey <quimeymata@gmail.com>
2023-02-12 01:41:52 -0300

    Adds different layouts according to step

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
+import PersonalInfoForm from './PersonalInfoForm/PersonalInfoForm';
+import SelectingPlan from './SelectingPlan/SelectingPlan';
+import AddOns from './AddOns/AddOns';
+import FinishingUp from './FinishingUp/FinishingUp';
 import Container from '../UI/Container/Container';
 import classes from './MainContent.module.css';
 
@@ -26,14 +31,19 @@ const stepsTitleAndSubtitle = [
   },
 ];
 
-const MainContent = ({ step }) => {
+const MainContent = () => {
+  const step = useSelector((state) => state.step.step);
+
   return (
     <Container className={classes['main-content']}>
       <h2 className={classes.title}>{stepsTitleAndSubtitle[step - 1].title}</h2>
       <p className={classes.subtitle}>
         {stepsTitleAndSubtitle[step - 1].subtitle}
       </p>
-      <p className={classes.remove}>Something according to current step</p>
+      {step === 1 && <PersonalInfoForm />}
+      {step === 2 && <SelectingPlan />}
+      {step === 3 && <AddOns />}
+      {step === 4 && <FinishingUp />}
     </Container>
   );
 };
